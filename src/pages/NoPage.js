a64c7ba8e0abca2b51b61ff0c57cac81170e392f
LAKAZET <lazolakivukadinovic1001@gmail.com>
2022-06-18 15:40:49 +0200

    nepostojeca stranica edit

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,5 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 const NoPage = () => {
-    return <h1>404</h1>;
+    return (
+      <>
+        <h1 className='text-center'>Error 404</h1>
+        <h3 className='text-center'>Nepostojeca stranica</h3>
+      </>
+    );
   };
   
   export default NoPage;
